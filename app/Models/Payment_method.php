bf7c51dc080d9df509e1ec378e3369d578b0ebb3
Urtzi010 <ulusarretaga22wg@ikzubirimanteo.com>
2023-12-11 13:00:16 +0100

    Models created

---
+++ app/Models/Payment_method.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment_method extends Model
+{
+    use HasFactory;
+    protected $table = 'payment_methods';
+    protected $fillable = ['number', 'owner_name', 'cvv', 'expire_date', 'user_id'];
+}
