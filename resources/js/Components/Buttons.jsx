08c07f2ee780fec7c2f53d9f81320a02eda2844e
ibanjara <ijarapu22wg@ikzubirimanteo.com>
2023-12-12 09:38:44 +0100

    Bouncing button fixed

--- resources/js/Components/Buttons.jsx
+++ resources/js/Components/Buttons.jsx
@@ -150,14 +150,11 @@ GlowSubmitButton.propTypes = {
 
 const BouncingButton = () => {
     return (
-        
-        <div className="animate-bounce mb-[50px] lg:mt-[100px] bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 self-center ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
-            <a href="#thanks">
-                <svg className="w-6 h-6 text-violet-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                </svg>
-            </a>
-        </div>
+        <a href="#thanks" class="animate-bounce mb-[50px] bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 self-center ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+            <svg class="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+            </svg>
+        </a>
     )
 }
 
