861e532e8101b9b7163c6ecfa7cbd7f35e404e90
enegalan <enekogalanelorza@gmail.com>
2023-12-16 18:03:55 +0100

    ProductCard colors update

--- resources/js/Components/ProductCard.jsx
+++ resources/js/Components/ProductCard.jsx
@@ -5,18 +5,18 @@ export default function ProductCard({image, title, price}){
     return (
         <div className="border relative z-10 max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-100 flex flex-col hover:[&>img]:scale-105">
             <div className="flex  justify-end pr-5 pt-5">
-                <a href="#"><i className="text-xl fas fa-heart transition duration-500 text-blue-700 hover:text-blue-900"></i></a>
+                <a href="#"><i className="text-xl fas fa-heart transition duration-500 text-[var(--main-blue)] hover:text-blue-900"></i></a>
             </div>
             <img className="w-full cursor-pointer transition ease-in delay-400" src={image} alt={title} />
             <div className="px-6 pt-4">
                 <div className="flex justify-between font-bold text-xl mb-2">
                     <span>{title}</span>
-                    <a href="#"><i className="text-xl fas fa-cart-plus transition duration-500 text-blue-700 hover:text-blue-900"></i></a>
+                    <a href="#"><i className="text-xl fas fa-cart-plus transition duration-500 text-[var(--main-blue)] hover:text-blue-900"></i></a>
                 </div>
                 <hr />
             </div>
             <div className="px-6 pb-4 pt-2">
-                <div className="font-bold text-xl mb-2">{price}€</div>
+                <div className="text-[var(--main-blue)] font-bold text-xl mb-2">{price}€</div>
             </div>
             {/* TODO: If there is an offer show the discount amount */}
             <div className="px-6 pt-4 pb-4 flex-grow flex justify-center items-center">
