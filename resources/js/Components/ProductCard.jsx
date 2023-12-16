e6d63dca89b5fab110d911a623de8afad0d88ba8
enegalan <enekogalanelorza@gmail.com>
2023-12-16 18:01:11 +0100

    ProductCard style update

--- resources/js/Components/ProductCard.jsx
+++ resources/js/Components/ProductCard.jsx
@@ -4,15 +4,18 @@ import PropTypes from "prop-types";
 export default function ProductCard({image, title, price}){
     return (
         <div className="border relative z-10 max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-100 flex flex-col hover:[&>img]:scale-105">
-            <div className="flex  justify-end gap-5 pr-5 pt-5">
-                <a href="#"><i className="text-xl fas fa-cart-plus"></i></a>
-                <a href="#"><i className="fas fa-heart"></i></a>
+            <div className="flex  justify-end pr-5 pt-5">
+                <a href="#"><i className="text-xl fas fa-heart transition duration-500 text-blue-700 hover:text-blue-900"></i></a>
             </div>
             <img className="w-full cursor-pointer transition ease-in delay-400" src={image} alt={title} />
             <div className="px-6 pt-4">
-                <div className="font-bold text-xl mb-2">{title}</div>
+                <div className="flex justify-between font-bold text-xl mb-2">
+                    <span>{title}</span>
+                    <a href="#"><i className="text-xl fas fa-cart-plus transition duration-500 text-blue-700 hover:text-blue-900"></i></a>
+                </div>
+                <hr />
             </div>
-            <div className="px-6 pb-4">
+            <div className="px-6 pb-4 pt-2">
                 <div className="font-bold text-xl mb-2">{price}€</div>
             </div>
             {/* TODO: If there is an offer show the discount amount */}
