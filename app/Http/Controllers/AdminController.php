6af2c300e9a35180fa9423d011c00b78f1fea628
ibanjara <ijarapu22wg@ikzubirimanteo.com>
2024-01-11 09:37:58 +0100

    Regions list view for admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Color;
+use App\Models\Country;
+use App\Models\Material;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Region;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 use Inertia\Inertia;
@@ -18,4 +25,65 @@ class AdminController extends Controller
             Inertia::render('Admin/Dashboard')
         );
     }
+
+    public function countries() {
+        app()->call([UserController::class, 'getRoles']);
+        $countries = Country::all();
+        return (
+            Inertia::render('Admin/Countries', ['countries' => $countries])
+        );
+    }
+
+    private $productPerPagination = 15;
+
+    public function regions() {
+        app()->call([UserController::class, 'getRoles']);
+        $regions = Region::paginate($this->productPerPagination);
+        $regions->load('country');
+        
+        return (
+            Inertia::render('Admin/Regions', ['regions' => $regions])
+        );
+    }
+
+    public function products() {
+        app()->call([UserController::class, 'getRoles']);
+        $products = Product::all();
+        return (
+            Inertia::render('Admin/Products', ['products' => $products])
+        );
+    }
+
+    public function users() {
+        app()->call([UserController::class, 'getRoles']);
+        $users = User::all();
+        return (
+            Inertia::render('Admin/Users', ['users' => $users])
+        );
+    }
+
+    public function orders() {
+        app()->call([UserController::class, 'getRoles']);
+        $orders = Order::all();
+        return (
+            Inertia::render('Admin/Orders', ['orders' => $orders])
+        );
+    }
+
+    public function materials() {
+        app()->call([UserController::class, 'getRoles']);
+        $materials = Material::all();
+        return (
+            Inertia::render('Admin/Materials', ['materials' => $materials])
+        );
+    }
+
+    public function colors() {
+        app()->call([UserController::class, 'getRoles']);
+        $colors = Color::all();
+        return (
+            Inertia::render('Admin/Colors', ['colors' => $colors])
+        );
+    }
+
 }
--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -1,26 +1,112 @@
-import '@/App.css';
+import { Link } from "@inertiajs/react";
+import logoBlue from "../../../../public/logoWhite.svg";
+import gbBlue from "../../../../public/images/Blue-dotted-Background-images.jpg"
+import { FaUser, FaHeart,FaWallet,FaAddressCard } from "react-icons/fa";
+import { FaCartShopping, FaGear } from "react-icons/fa6";
 
-import NavBar from '@/Components/NavBar';
-import { Footer } from '@/Components/Footer';
-
-function Dashboard({ auth }) {
-
-    return (
-        <>
-            <div className='overflow-hidden bg-[var(--light-grey)]'>
-                <NavBar user={auth.user} dynamicBackground={false} defaultBackgroundColor='var(--main-blue)' defaultTextColor='white' />
-                <section className='my-48'>
-                    <h1 className='font-bold text-3xl'>Admin Dashboard</h1>
-                    <div>
-                        <iframe title="PrintHub" width="1140" height="541.25"
-                        src="https://app.powerbi.com/reportEmbed?reportId=9e0afcf1-ec2b-4b1f-8642-14cd8d0147da&autoAuth=true&ctid=d588ea47-59c1-4a51-b7c3-06ed08676111"
-                        frameborder="0" allowFullScreen="true"></iframe>
-                    </div>
-                </section>
-                <Footer />
+export default function ProfileLayout({ children,pageName='Dashboard' }) {
+  return (
+    <main className={`bg-[url('${gbBlue}')] bg-cover`}>
+      <div className="h-screen flex bg-white/30">
+        <nav className="w-52 flex flex-col">
+          <div className="p-2 flex justify-center">
+            <img src={logoBlue} alt="Logo" />
+          </div>
+          <ul className="flex flex-col gap-2 mt-10 text-xl flex-1">
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.dashboard")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaUser className="text-blue-700" />
+                <p className="">Dashboard</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.products")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaCartShopping className="text-blue-700" />
+                <p>Products</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.users")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaHeart className="text-blue-700" />
+                <p>Users</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.orders")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaAddressCard className="text-blue-700" />
+                <p>Orders</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.materials")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaWallet className="text-blue-700" />
+                <p>Materials</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.colors")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaWallet className="text-blue-700" />
+                <p>Colors</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.countries")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaWallet className="text-blue-700" />
+                <p>Countries</p>
+              </Link>
+            </li>
+            <li className="hover:bg-blue-100 py-2">
+              <Link
+                href={route("admin.regions")}
+                className="flex gap-2 items-center pl-5"
+              >
+                <FaWallet className="text-blue-700" />
+                <p>Regions</p>
+              </Link>
+            </li>
+          </ul>
+          <div className="flex pb-4 pl-4 justify-start">
+            <Link
+              href={route("index")}
+              className=""
+            >
+              <FaGear className="text-blue-700 text-4xl" />
+            </Link>
+          </div>
+        </nav>
+        <div className="flex flex-col flex-1">
+          <div className="my-4">
+            <h1 className="text-4xl">Admin {pageName}</h1>
+            <p className="text-gray-700">Welcome to the admin section</p>
+          </div>
+          <div className="bg-gray-200/60 overflow-auto rounded-xl px-10 py-10 flex-1 mb-14 flex flex-col mr-5">
+            <div className="">
+              {children}
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
-
-export default Dashboard;
