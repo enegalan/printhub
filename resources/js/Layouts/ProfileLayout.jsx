0f673c164acebc688a1b47b311a850b20993783e
Urtzi010 <ulusarretaga22wg@ikzubirimanteo.com>
2024-01-16 08:02:40 +0100

    Provider edit product fix

--- resources/js/Layouts/ProfileLayout.jsx
+++ resources/js/Layouts/ProfileLayout.jsx
@@ -7,26 +7,25 @@ import {
   FaWallet,
   FaAddressCard,
   FaHome,
+  FaBars
 } from "react-icons/fa";
 import { AiFillDashboard } from "react-icons/ai";
 import { FaCartShopping, FaGear, FaHandshake } from "react-icons/fa6";
-import { useState } from "react";
 
 export default function ProfileLayout({
   children,
   pageName = "Dashboard",
   user,
 }) {
-
   var isProvider = false;
   if (user && user.hasOwnProperty("roles"))
     isProvider = user.roles.some((role) => role.name === "provider");
 
   return (
-    <main className={`bg-[url('${gbBlue}')] bg-cover`}>
+    <main className={`bg-[url('${gbBlue}')] bg-auto`} >
       <div className="h-screen flex bg-white/30">
-        <nav className="w-52 flex flex-col">
-          <div className="p-2 flex justify-center">
+        <nav className="w-52 flex flex-col justify-center">
+          <div className="mt-4 flex justify-center">
             <Link href={route("index")}>
               <img src={logoBlue} alt="Logo" />
             </Link>
@@ -35,7 +34,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("index")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaHome className="text-blue-700" />
                 <p className="">Home</p>
@@ -44,7 +43,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("profile.dashboard")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <AiFillDashboard className="text-blue-700" />
                 <p className="">Dashboard</p>
@@ -53,7 +52,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("profile.edit")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaUser className="text-blue-700" />
                 <p className="">Profile</p>
@@ -62,7 +61,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("index")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaCartShopping className="text-blue-700" />
                 <p>Orders</p>
@@ -71,7 +70,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("index")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaHeart className="text-blue-700" />
                 <p>Wishlist</p>
@@ -80,7 +79,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("index")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaAddressCard className="text-blue-700" />
                 <p>Address</p>
@@ -89,7 +88,7 @@ export default function ProfileLayout({
             <li className="hover:bg-blue-100 py-2">
               <Link
                 href={route("index")}
-                className="flex gap-2 items-center pl-5"
+                className="flex gap-2 items-center px-5"
               >
                 <FaWallet className="text-blue-700" />
                 <p>Payment</p>
@@ -99,7 +98,7 @@ export default function ProfileLayout({
               <li className="hover:bg-blue-100 py-2">
                 <Link
                   href={route("profile.provider")}
-                  className="flex gap-2 items-center pl-5 "
+                  className="flex gap-2 items-center px-5 "
                 >
                   <FaHandshake className="text-blue-700" />
                   <p>Provider</p>
@@ -107,13 +106,25 @@ export default function ProfileLayout({
               </li>
             )}
           </ul>
+          <div className="self-center mb-5">
+            <Link
+              href={route("logout")}
+              method="post"
+              as="button"
+              className="text-white hover:underline text-lg bg-blue-700 py-1 px-4 rounded font-bold"
+            >
+              Log out
+            </Link>
+          </div>
         </nav>
         <div className="flex flex-col flex-1">
-          <div className="my-4">
-            <h1 className="text-4xl">Profile {pageName}</h1>
-            <p className="text-gray-700">Welcom to the profile section</p>
+          <div className="my-4 flex flex-col md:flex-row justify-between md:items-center md:mr-5">
+            <div className="mb-4 md:mb-0">
+              <h1 className="text-4xl">Profile {pageName}</h1>
+              <p className="text-gray-700">Welcom to the profile section</p>
+            </div>
           </div>
-          <div className="bg-gray-200/60 rounded-xl px-10 py-10 flex-1 mb-14 flex flex-col mr-5 overflow-auto">
+          <div className="bg-gray-200/60 rounded-xl px-10 py-10 mb-8 md:mb-14 flex-1 flex flex-col mr-4 md:mr-5 overflow-auto">
             {children}
           </div>
         </div>
--- resources/js/Pages/Provider/Edit.jsx
+++ resources/js/Pages/Provider/Edit.jsx
@@ -8,177 +8,195 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { Link } from "@inertiajs/react";
 
 export default function ProviderDashboard({ user, product, categories = [] }) {
-    const { data, setData, put, processing, errors, reset } = useForm({
-        name: product.name,
-        description: product.description,
-        image: null,
-        price: product.price,
-        categories: [],
-        user_id: user.id,
-    });
-    const [previewUrl, setPreviewUrl] = useState(`/storage/products/${product.image}`);
-    const handleFileChange = (e) => {
-        const selectedImage = e.target.files[0];
-        setData("image", selectedImage);
+  const { data, setData, put, processing, errors, reset } = useForm({
+    name: product.name,
+    description: product.description,
+    image: null,
+    price: product.price,
+    categories: [],
+    user_id: user.id,
+  });
+  const [previewUrl, setPreviewUrl] = useState(
+    `/storage/products/${product.image}`
+  );
+  const handleFileChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setData("image", selectedImage);
 
-        // Create an object URL for the preview
-        const objectUrl = URL.createObjectURL(selectedImage);
-        setPreviewUrl(objectUrl);
+    // Create an object URL for the preview
+    const objectUrl = URL.createObjectURL(selectedImage);
+    setPreviewUrl(objectUrl);
+  };
+  useEffect(() => {
+    return () => {
+      if (previewUrl) {
+        URL.revokeObjectURL(previewUrl);
+      }
     };
-    useEffect(() => {
-        return () => {
-            if (previewUrl) {
-                URL.revokeObjectURL(previewUrl);
-            }
-        };
-    }, [previewUrl]);
+  }, [previewUrl]);
+  
+  const submit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append(
+      "image",
+      data.image !== "" || data.image !== null ? data.image : image
+    );
+    formData.append("price", data.price);
+    formData.append("categories", data.categories);
+    formData.append("user_id", data.user_id);
+    formData.append("product_id", data.product_id);
 
-    const submit = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("name", data.name);
-        formData.append("description", data.description);
-        formData.append("image", data.image !== "" || data.image !== null ? data.image : image);
-        formData.append("price", data.price);
-        formData.append("categories", data.categories);
-        formData.append("user_id", data.user_id);
-        formData.append("product_id", data.product_id);
-    
-        put(route("product.update", product));
-    };
+    put(route("product.update", product));
+  };
 
-    return (
-        <ProfileLayout user={user} pageName="provider">
-            <Link href={route('profile.provider')} className="bg-[lightgrey] w-[40px] p-3 rounded-lg mb-5 self-start transition hover:bg-[#bbbbbb]">
-                <IoMdArrowRoundBack />
-            </Link>
-            <h1 className="text-2xl mb-5 text-center">Edit product</h1>
-            <div className="row-span-4 bg-white rounded-xl p-4 lg:mx-20">
-                <form
-                    className="px-10 py-5 flex flex-col gap-5"
-                    onSubmit={submit}
-                    encType="multipart/form-data"
-                >
-                    <div>
-                        <InputLabel forInput="name" value="Product name" className="" />
-                        <TextInput
-                            id="name"
-                            name="name"
-                            value={data.name}
-                            className="mt-1 block w-full"
-                            autoComplete="name"
-                            isFocused={true}
-                            onChange={(e) => setData("name", e.target.value)}
-                        />
-                        <InputError message={errors.name} className="mt-2" />
-                    </div>
-                    <div>
-                        <InputLabel value="Description" className="" />
-                        <textarea
-                            id="description"
-                            name="description"
-                            value={data.description}
-                            className="mt-1 block w-full border-gray-300 focus:border-blue-400 focus:ring-blue-400 rounded-md shadow-sm"
-                            autoComplete="description"
-                            isFocused={true}
-                            onChange={(e) => setData("description", e.target.value)}
-                            rows="5"
-                            placeholder="Maximun 255 chararcters"
-                        />
-                        <InputError message={errors.description} className="mt-2" />
-                    </div>
-                    <div>
-                        <InputLabel
-                            forInput="image"
-                            value="Choose an image"
-                            className="font-medium text-gray-900"
-                        />
-                        <TextInput
-                            id="image"
-                            type="file"
-                            name="image"
-                            className="mt-1 block shadow-transparent text-slate-500
+  const handleCheck = (category)=>{
+    for (const categoryP of product.categories) {
+        if (category.id === categoryP.id) {
+          return true
+        }
+      }
+      return false
+  }
+
+  return (
+    <ProfileLayout user={user} pageName="provider">
+      <Link
+        href={route("profile.provider")}
+        className="bg-[lightgrey] w-[40px] p-3 rounded-lg mb-5 self-start transition hover:bg-[#bbbbbb]"
+      >
+        <IoMdArrowRoundBack />
+      </Link>
+      <h1 className="text-2xl mb-5 text-center">Edit product</h1>
+      <div className="row-span-4 bg-white rounded-xl p-4 lg:mx-20">
+        <form
+          className="px-10 py-5 flex flex-col gap-5"
+          onSubmit={submit}
+          encType="multipart/form-data"
+        >
+          <div>
+            <InputLabel forInput="name" value="Product name" className="" />
+            <TextInput
+              id="name"
+              name="name"
+              value={data.name}
+              className="mt-1 block w-full"
+              autoComplete="name"
+              isFocused={true}
+              onChange={(e) => setData("name", e.target.value)}
+            />
+            <InputError message={errors.name} className="mt-2" />
+          </div>
+          <div>
+            <InputLabel value="Description" className="" />
+            <textarea
+              id="description"
+              name="description"
+              value={data.description}
+              className="mt-1 block w-full border-gray-300 focus:border-blue-400 focus:ring-blue-400 rounded-md shadow-sm"
+              autoComplete="description"
+              isFocused={true}
+              onChange={(e) => setData("description", e.target.value)}
+              rows="5"
+              placeholder="Maximun 255 chararcters"
+            />
+            <InputError message={errors.description} className="mt-2" />
+          </div>
+          <div>
+            <InputLabel
+              forInput="image"
+              value="Choose an image"
+              className="font-medium text-gray-900"
+            />
+            <TextInput
+              id="image"
+              type="file"
+              name="image"
+              className="mt-1 block shadow-transparent text-slate-500
                 file:mr-4 file:py-2 file:px-10
                 file:rounded-full file:border-0
                 file:text-sm file:font-semibold
                 file:bg-blue-50 file:text-blue-700
                 hover:file:bg-blue-100 "
-                            autoComplete="image"
-                            isFocused={true}
-                            onChange={handleFileChange}
-                        />
-                        {previewUrl && (
-                            <img
-                                src={previewUrl}
-                                alt="Image Preview"
-                                className="mt-2 w-32 h-32"
-                            />
-                        )}
-                        <InputError message={errors.image} className="mt-2" />
-                    </div>
-                    <div>
-                        <InputLabel
-                            forInput="price"
-                            value="Set price"
-                            className="font-medium text-gray-900"
-                        />
-                        <div className="flex gap-2">
-                            <TextInput
-                                id="price"
-                                type="number"
-                                step="0.1"
-                                name="price"
-                                value={data.price}
-                                className="mt-1 w-full"
-                                autoComplete="price"
-                                isFocused={true}
-                                onChange={(e) => setData("price", e.target.value)}
-                            />
-                        </div>
-                        <InputError message={errors.price} className="mt-2" />
-                    </div>
-                    <div>
-                        <InputLabel
-                            forInput="categories"
-                            value="Choose categories"
-                            className="font-medium text-gray-900"
-                        />
-                        <div className="flex gap-2 flex-col">
-                            {categories.map((category, index) => (
-                                <div key={index} className="flex gap-2">
-                                    <TextInput
-                                        id={`category-${category.id}`}
-                                        type="checkbox"
-                                        name="categories[]"
-                                        value={data.categories.includes(category.id)}
-                                        className="mt-1"
-                                        autoComplete={`categories-${category.id}`}
-                                        isFocused={true}
-                                        onChange={() => {
-                                            setData(
-                                                "categories",
-                                                data.categories.includes(category.id)
-                                                    ? data.categories.filter((id) => id !== category.id)
-                                                    : [...data.categories, category.id]
-                                            );
-                                        }}
-                                        checked={data.categories.includes(category.id)}
-                                    />
-                                    <label htmlFor={`category-${category.id}`}>
-                                        {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
-                                    </label>
-                                </div>
-                            ))}
-                        </div>
-                        <InputError message={errors.categories} className="mt-2" />
-                    </div>
-                    <div>
-                        <button className="rounded bg-blue-500 text-white px-5 py-2 font-medium">
-                            Submit product
-                        </button>
-                    </div>
-                </form>
+              autoComplete="image"
+              isFocused={true}
+              onChange={handleFileChange}
+            />
+            {previewUrl && (
+              <img
+                src={previewUrl}
+                alt="Image Preview"
+                className="mt-2 w-32 h-32"
+              />
+            )}
+            <InputError message={errors.image} className="mt-2" />
+          </div>
+          <div>
+            <InputLabel
+              forInput="price"
+              value="Set price"
+              className="font-medium text-gray-900"
+            />
+            <div className="flex gap-2">
+              <TextInput
+                id="price"
+                type="number"
+                step="0.1"
+                name="price"
+                value={data.price}
+                className="mt-1 w-full"
+                autoComplete="price"
+                isFocused={true}
+                onChange={(e) => setData("price", e.target.value)}
+              />
             </div>
-        </ProfileLayout>
-    );
+            <InputError message={errors.price} className="mt-2" />
+          </div>
+          <div>
+            <InputLabel
+              forInput="categories"
+              value="Choose categories"
+              className="font-medium text-gray-900"
+            />
+            <div className="flex gap-2 flex-col">
+              {categories.map((category, index) => (
+                <div key={index} className="flex gap-2">
+                  <TextInput
+                    id={`category-${category.id}`}
+                    type="checkbox"
+                    name="categories[]"
+                    value={data.categories.includes(category.id)}
+                    className="mt-1"
+                    autoComplete={`categories-${category.id}`}
+                    isFocused={true}
+                    checked={data.categories.includes(category.id) || handleCheck()}
+                    onChange={() => {
+                      setData(
+                        "categories",
+                        data.categories.includes(category.id)
+                          ? data.categories.filter((id) => id !== category.id)
+                          : [...data.categories, category.id]
+                      );
+                    }}
+                  />
+                  <label htmlFor={`category-${category.id}`}>
+                    {category.name.charAt(0).toUpperCase() +
+                      category.name.slice(1)}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <InputError message={errors.categories} className="mt-2" />
+          </div>
+          <div>
+            <button className="rounded bg-blue-500 text-white px-5 py-2 font-medium">
+              Submit product
+            </button>
+          </div>
+        </form>
+      </div>
+    </ProfileLayout>
+  );
 }
