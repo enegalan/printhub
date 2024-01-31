6d75a75fc047a26288d909caa7ed5f47db256b80
enegalan <enekogalanelorza@gmail.com>
2024-01-31 13:00:40 +0100

    Select Options fix & Chart.js in Admin Dashboard

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -1,10 +1,141 @@
 import { Link } from "@inertiajs/react";
 import logoBlue from "../../../../public/logoWhite.svg";
 import gbBlue from "../../../../public/images/Blue-dotted-Background-images.jpg"
-import { FaUser, FaHeart,FaWallet,FaAddressCard, FaHome } from "react-icons/fa";
+import { FaUser, FaHeart, FaWallet, FaAddressCard, FaHome } from "react-icons/fa";
 import { FaCartShopping, FaGear } from "react-icons/fa6";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 
-export default function ProfileLayout({ children, pageName='Admin Dashboard', pageSubtitle='Welcome to admin section' }) {
+
+import React, { useEffect } from 'react';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement,
+);
+
+export default function ProfileLayout({ children, pageName = 'Admin Dashboard', pageSubtitle = 'Welcome to admin section' }) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'December'];
+  const daysInMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0
+  ).getDate();
+  const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
+  
+  // Orders this year
+  const ordersOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Orders this year',
+      },
+    },
+  };
+  const ordersData = {
+    labels: months,
+    datasets: [
+      {
+        fill: true,
+        label: 'Orders',
+        data: [100, 200, 500, 1000, 600, 700, 456, 1020, 2000, 1230,],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  // Registered users this month
+  const usersOptions = {
+    responsive: true,
+    redraw: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Authenticated users this month',
+      },
+    },
+  };
+  const usersData = {
+    labels: days,
+    datasets: [
+      {
+        fill: true,
+        label: 'Users',
+        data: [100, 200, 500, 1000, 600, 700, 456, 1020, 2000, 1230, 2000, 2123, 1232 , 1251, 1234, 555, 23, 155, 200, 300, 500, 250, 700, 200, 300, 500, 670, 300, 200, 150, 200],
+        borderColor: '#8ad58a',
+        backgroundColor: '#8ad58a69',
+      },
+    ],
+  };
+
+  // Orders by zone this year
+  const ordersZoneOptions = {
+    responsive: true,
+    redraw: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Orders by zone this year',
+      },
+    },
+  };
+  const ordersZoneData = {
+    labels: ['Spain', 'UK', 'Russia', 'Germany', 'Poland', 'Indonesia'],
+    datasets: [
+      {
+        label: 'Zone',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <main className={`bg-[url('${gbBlue}')] bg-cover`}>
       <div className="h-screen flex bg-white/30">
@@ -122,10 +253,12 @@ export default function ProfileLayout({ children, pageName='Admin Dashboard', pa
             <h1 className="text-4xl">{pageName}</h1>
             <p className="text-gray-700">{pageSubtitle}</p>
           </div>
-          <div className="bg-gray-200/60 overflow-auto rounded-xl px-10 py-10 flex-1 mb-14 flex flex-col mr-5">
-            
-              {children}
-            
+          <div className="bg-gray-200/60 overflow-auto rounded-xl px-10 py-10 flex-1 mb-14 flex flex-col mr-5">  
+            <h3 className="text-2xl font-bold">Reports</h3>
+            <Line options={ordersOptions} data={ordersData} />
+            <Line options={usersOptions} data={usersData} />
+            <Pie options={ordersZoneOptions} data={ordersZoneData} />
+            {children}
           </div>
         </div>
       </div>
