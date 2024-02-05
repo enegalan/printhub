a73e0e2f20456e8d2504a5a77914d98dc0edb51a
ibanjara <ijarapu22wg@ikzubirimanteo.com>
2024-02-05 09:40:00 +0100

    Added colors to order status and removed add button from order in admin dashboard

--- resources/js/Pages/Admin/Order/Orders.jsx
+++ resources/js/Pages/Admin/Order/Orders.jsx
@@ -5,12 +5,10 @@ import { Link } from '@inertiajs/react';
 import { FaPlus } from 'react-icons/fa';
 export default function ({ orders }) {
   const { data, prev_page_url, next_page_url, current_page, last_page } = orders;
+
   return (
     <Dashboard pageName='Orders' pageSubtitle='Checkout the orders'>
       <div className='flex flex-col min-h-full '>
-        <Link href={route('provider.add')} className="bg-[lightgrey] w-[40px] p-3 rounded-lg mb-5 self-end transition hover:bg-[#bbbbbb]">
-          <FaPlus />
-        </Link>
         <div className='overflow-x-scroll'>
           <table className="min-w-full bg-white border border-gray-300">
             <thead>
@@ -33,7 +31,7 @@ export default function ({ orders }) {
                     <td className="py-2 px-4 border-b">{order.invoice.name}</td>
                     <td className="py-2 px-4 border-b">{order.invoice.lastname}</td>
                     <td className="py-2 px-4 border-b">{order.invoice.email}</td>
-                    <td className="py-2 px-4 border-b">{order.status}</td>
+                    <td className={`py-2 px-4 border-b ${order.status === 'Paid' ? 'bg-green-100' : order.status === 'Not paid' ? 'bg-red-100' : order.status === 'Shipping' ? 'bg-orange-100' : order.status === 'Delivered' ? 'bg-blue-100' : ''}`}>{order.status}</td>
                     <td className="py-2 px-4 border-b">
                       <Link
                         href={route('admin.order.view', order.id)}
