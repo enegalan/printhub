2ac2c4da8559df779b2e01bf461c15239bf73e2f
Urtzi010 <ulusarretaga22wg@ikzubirimanteo.com>
2024-02-05 12:38:51 +0100

    Latest products section in index

--- resources/js/Components/sections/LatestProductsSection.jsx
+++ resources/js/Components/sections/LatestProductsSection.jsx
@@ -1,30 +1,7 @@
 
-import piece from '../../../../public/images/imagen1.png';
-
 import ProductCard from "@/Components/ProductCard";
-export default function LatestProductsSection(){
-    const marketProducts = [
-        {
-            image: piece,
-            title: "Reviving Retro PCs",
-            price: 5.99,
-        },
-        {
-            image: piece,
-            title: "Another Title",
-            price: 5.99,
-        },
-        {
-            image: piece,
-            title: "One More Title",
-            price: 5.99,
-        },
-        {
-            image: piece,
-            title: "One More Title",
-            price: 5.99,
-        },
-    ];
+export default function LatestProductsSection({latestProducts}){
+    console.log(latestProducts);
     return(
         <section id="partsSectionIndex" className="relative z-10 bg-[var(--light-grey)] pt-20">
             <div className="mx-2 flex flex-col items-center">
@@ -32,11 +9,11 @@ export default function LatestProductsSection(){
                 <h1 className="text-4xl font-bold text-blue-800">WITH THE BEST SERVICE</h1>
             </div>
             <div className="mt-10 pb-10 grid justify-center md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-4 md:mx-20 mx-3">
-                {marketProducts.map((item, index) => (
+                {latestProducts.map((item, index) => (
                     <ProductCard
                         key={index}
                         image={item.image}
-                        title={item.title}
+                        name={item.name}
                         price={item.price}
                         colors={[]}
                     />
