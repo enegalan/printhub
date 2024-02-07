802e31259f26515476e4c3c039318e11d87d9db6
ibanjara <ijarapu22wg@ikzubirimanteo.com>
2024-02-07 11:41:11 +0100

    Responsive fix

--- resources/js/Components/sections/LatestProductsSection.jsx
+++ resources/js/Components/sections/LatestProductsSection.jsx
@@ -7,7 +7,7 @@ export default function LatestProductsSection({latestProducts}){
                 <h1 className="text-3xl text-blue-800">GET THE HIGHEST QUALITY FOR YOUR 3D PARTS</h1>
                 <h1 className="text-4xl font-bold text-blue-800">WITH THE BEST SERVICE</h1>
             </div>
-            <div className="mt-10 pb-10 grid justify-center md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-4 md:mx-20 mx-3">
+            <div className="mt-10 pb-10 grid justify-items-center justify-center md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-4 md:mx-20 mx-3">
                 {latestProducts.map((item, index) => (
                     <ProductCard
                         key={index}
